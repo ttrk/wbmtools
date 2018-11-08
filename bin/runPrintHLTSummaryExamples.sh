cb96d657a3f6b568e372fdacfcd447b664a315fa
Kaya Tatar <tatark@mit.edu>
2018-11-08 18:00:51 +0100

    fix

--- bin/runPrintHLTSummaryExamples.sh
+++ bin/runPrintHLTSummaryExamples.sh
@@ -19,7 +19,7 @@ set -x
 mkdir -p test/
 
 ## example 1
-# print all L1 summary columns for given comma separated list of paths
+# print all HLT summary columns for given comma separated list of paths
 ./printHLTSummary.py --run 326262 --pathnames HLT_HIGEDPhoton10_v1,HLT_HIGEDPhoton20_v1,HLT_HIGEDPhoton30_v1 &> test/example1HLT.log
 
 ## example 2
@@ -27,12 +27,12 @@ mkdir -p test/
 ./printHLTSummary.py --run 263322 --pathnames HLT_HIPuAK4CaloJet40_Eta5p1_v1,HLT_HIPuAK4CaloJet60_Eta5p1_v1,HLT_HIPuAK4CaloJet80_Eta5p1_v1 --colIndex 2,4,5,6,7 &> test/example2HLT.log
 
 ## example 3
-# L1 paths to search are in a txt file
+# HLT paths to search are in a txt file
 # print first 4 columns from HLTSummary table
 ./printHLTSummary.py --run 326238 --pathnames test/HLTpaths.txt --colIndex 1,2,3,4,5,6 &> test/example3HLT.log
 
 ## example 4
-# L1 paths to search are in a txt file
+# HLT paths to search are in a txt file
 # print first 6 columns from HLTSummary table
 # save the output to a csv file
 ./printHLTSummary.py --run 326262 --pathnames test/HLTpaths.txt --colIndex 1,2,3,4,5,6,7 --outcsv test/example4HLT.csv &> test/example4HLT.log
