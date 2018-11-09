b18c99422e9208daa059382172797211b438f7e6
Kaya Tatar <tatark@mit.edu>
2018-11-09 16:42:04 +0100

    rearrange output for L1 and HLT summary

--- bin/runPrintHLTSummaryExamples.sh
+++ bin/runPrintHLTSummaryExamples.sh
@@ -16,30 +16,30 @@ set -x
 # 9     : PReject
 # 10    : L1 Prerequisite
 
-mkdir -p test/
+mkdir -p test_HLTSummary/
 
 ## example 1
 # print all HLT summary columns for given comma separated list of paths
-./printHLTSummary.py --run 326262 --pathnames HLT_HIGEDPhoton10_v1,HLT_HIGEDPhoton20_v1,HLT_HIGEDPhoton30_v1 &> test/example1HLT.log
+./printHLTSummary.py --run 326262 --pathnames HLT_HIGEDPhoton10_v1,HLT_HIGEDPhoton20_v1,HLT_HIGEDPhoton30_v1 &> test_HLTSummary/example1.log
 
 ## example 2
 # print 2nd,5th, and 6th column from HLTSummary table
-./printHLTSummary.py --run 263322 --pathnames HLT_HIPuAK4CaloJet40_Eta5p1_v1,HLT_HIPuAK4CaloJet60_Eta5p1_v1,HLT_HIPuAK4CaloJet80_Eta5p1_v1 --colIndex 2,4,5,6,7 &> test/example2HLT.log
+./printHLTSummary.py --run 263322 --pathnames HLT_HIPuAK4CaloJet40_Eta5p1_v1,HLT_HIPuAK4CaloJet60_Eta5p1_v1,HLT_HIPuAK4CaloJet80_Eta5p1_v1 --colIndex 2,4,5,6,7 &> test_HLTSummary/example2.log
 
 ## example 3
 # HLT paths to search are in a txt file
 # print first 6 columns from HLTSummary table
-./printHLTSummary.py --run 326238 --pathnames test/HLTpaths.txt --colIndex 1,2,3,4,5,6 &> test/example3HLT.log
+./printHLTSummary.py --run 326238 --pathnames test_HLTSummary/HLTpaths.txt --colIndex 1,2,3,4,5,6 &> test_HLTSummary/example3.log
 
 ## example 4
 # HLT paths to search are in a txt file
 # print first 7 columns from HLTSummary table
 # save the output to a csv file
-./printHLTSummary.py --run 326262 --pathnames test/HLTpaths.txt --colIndex 1,2,3,4,5,6,7 --outcsv test/example4HLT.csv &> test/example4HLT.log
+./printHLTSummary.py --run 326262 --pathnames test_HLTSummary/HLTpaths.txt --colIndex 1,2,3,4,5,6,7 --outcsv test_HLTSummary/example4.csv &> test_HLTSummary/example4.log
 
 ## example 5
 # HLT paths to search are in a txt file
 # print first 7 columns from HLTSummary table
 # save the output to a csv file
 # The two numbers following run number are min and max lumi sections
-./printHLTSummary.py --run 326262:105:479 --pathnames test/HLTpaths.txt --colIndex 1,2,3,4,5,6,7 --outcsv test/example5HLT.csv &> test/example5HLT.log
+./printHLTSummary.py --run 326262:105:479 --pathnames test_HLTSummary/HLTpaths.txt --colIndex 1,2,3,4,5,6,7 --outcsv test_HLTSummary/example5.csv &> test_HLTSummary/example5.log
--- bin/runPrintL1SummaryExamples.sh
+++ bin/runPrintL1SummaryExamples.sh
@@ -17,30 +17,30 @@ set -x
 # 10    : Final Prescale
 ## WARNING : For older runs the column structure is different than newer ones. Example : index=5 points to "Pre-DT Counts After Prescale" for run 326238. The same index points to "Pre-DT RMS Rate, Hz After Prescale" for run 263322.
 
-mkdir -p test/
+mkdir -p test_L1Summary/
 
 ## example 1
 # print all L1 summary columns for given comma separated list of paths
-./printL1Summary.py --run 326262 --pathnames L1_MinimumBiasHF1_AND_BptxAND,L1_SingleJet16_BptxAND,L1_SingleJet44_BptxAND &> test/example1.log
+./printL1Summary.py --run 326262 --pathnames L1_MinimumBiasHF1_AND_BptxAND,L1_SingleJet16_BptxAND,L1_SingleJet44_BptxAND &> test_L1Summary/example1.log
 
 ## example 2
 # print 2nd,5th, and 6th column from L1Summary table
-./printL1Summary.py --run 263322 --pathnames L1_ZeroBias,L1_MinimumBiasHF1_AND,L1_MinimumBiasHF2_AND --colIndex 2,5,6 &> test/example2.log
+./printL1Summary.py --run 263322 --pathnames L1_ZeroBias,L1_MinimumBiasHF1_AND,L1_MinimumBiasHF2_AND --colIndex 2,5,6 &> test_L1Summary/example2.log
 
 ## example 3
 # L1 paths to search are in a txt file
 # print first 4 columns from L1Summary table
-./printL1Summary.py --run 326238 --pathnames test/L1paths.txt --colIndex 1,2,3,4 &> test/example3.log
+./printL1Summary.py --run 326238 --pathnames test_L1Summary/L1paths.txt --colIndex 1,2,3,4 &> test_L1Summary/example3.log
 
 ## example 4
 # L1 paths to search are in a txt file
 # print first 6 columns from L1Summary table
 # save the output to a csv file
-./printL1Summary.py --run 326262 --pathnames test/L1paths.txt --colIndex 1,2,3,4,5,6 --outcsv test/example4.csv &> test/example4.log
+./printL1Summary.py --run 326262 --pathnames test_L1Summary/L1paths.txt --colIndex 1,2,3,4,5,6 --outcsv test_L1Summary/example4.csv &> test_L1Summary/example4.log
 
 ## example 5
 # L1 paths to search are in a txt file
 # print first 6 columns from L1Summary table
 # save the output to a csv file
 # The two numbers following run number are min and max lumi sections
-./printL1Summary.py --run 326262:250:540 --pathnames test/L1paths.txt --colIndex 1,2,3,4,5,6 --outcsv test/example5.csv &> test/example5.log
+./printL1Summary.py --run 326262:250:540 --pathnames test_L1Summary/L1paths.txt --colIndex 1,2,3,4,5,6 --outcsv test_L1Summary/example5.csv &> test_L1Summary/example5.log
